8d1c9367a214a96bb11c19d65d8402000be5817c
Byaya <45914657+Byaya@users.noreply.github.com>
2019-04-19 00:33:30 +0800

    第四次小组作业

---
+++ 小组任务/Cube_And_Fire/d3dUtil.h
@@ -0,0 +1,69 @@
+//***************************************************************************************
+// d3dUtil.h by X_Jun(MKXJun) (C) 2018-2019 All Rights Reserved.
+// Licensed under the MIT License.
+//
+// D3D实用工具集
+// Direct3D utility tools.
+//***************************************************************************************
+
+#ifndef D3DUTIL_H
+#define D3DUTIL_H
+
+#include <d3d11_1.h>			// 已包含Windows.h
+#include <DirectXCollision.h>	// 已包含DirectXMath.h
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <d3dcompiler.h>
+#include <wrl/client.h>
+#include <wincodec.h>
+#include "DXTrace.h"
+#include "DDSTextureLoader.h"
+#include "WICTextureLoader.h"
+#include <filesystem>
+#include <vector>
+#include <string>
+
+
+//
+// 宏相关
+//
+
+// 安全COM组件释放宏
+#define SAFE_RELEASE(p) { if ((p)) { (p)->Release(); (p) = nullptr; } }
+
+//
+// 着色器编译相关函数
+//
+
+// ------------------------------
+// CreateShaderFromFile函数
+// ------------------------------
+// [In]csoFileNameInOut 编译好的着色器二进制文件(.cso)，若有指定则优先寻找该文件并读取
+// [In]hlslFileName     着色器代码，若未找到着色器二进制文件则编译着色器代码
+// [In]entryPoint       入口点(指定开始的函数)
+// [In]shaderModel      着色器模型，格式为"*s_5_0"，*可以为c,d,g,h,p,v之一
+// [Out]ppBlobOut       输出着色器二进制信息
+HRESULT CreateShaderFromFile(
+	const WCHAR* csoFileNameInOut,
+	const WCHAR* hlslFileName,
+	LPCSTR entryPoint,
+	LPCSTR shaderModel,
+	ID3DBlob** ppBlobOut);
+
+//
+// 纹理数组相关函数
+//
+
+// 根据给定的DDS纹理文件集合，创建2D纹理数组
+// 要求所有纹理的宽度和高度都一致
+// 若maxMipMapSize为0，使用默认mipmap等级
+// 否则，mipmap等级将不会超过maxMipMapSize
+Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> CreateDDSTexture2DArrayFromFile(
+	Microsoft::WRL::ComPtr<ID3D11Device> device,
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext,
+	const std::vector<std::wstring>& filenames,
+	UINT maxMipMapSize = 0);
+
+
+
+#endif
